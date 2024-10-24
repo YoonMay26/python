4e43d860fe0160e23ea2a6c1f5aee281ad3922fb
YOON <ppom20152@gmail.com>
2024-10-24 14:28:14 +0900

    빼기 함수 추가

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 def add_func(n1,n2):
     return n1+n2
 
+def sub_func(n1,n2):
+    return n1-n2
 
 
 ## 전역 변수부
@@ -11,3 +13,6 @@ num1, num2, res = 100, 200, 0
 ## 메인 코드부
 res = add_func(num1, num2)
 print(num1, "+", num2, "=", res)
+
+res = sub_func(num1, num2)
+print(num1, "-", num2, "=", res)
