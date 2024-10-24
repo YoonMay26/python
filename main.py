b642214c61a9a0103ff0867445b712ff11bd7292
YOON <ppom20152@gmail.com>
2024-10-24 14:31:55 +0900

    나누기 함수 추가

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def sub_func(n1,n2):
 def gop_func(n1,n2):
     return n1*n2
 
+def div_func(n1,n2):
+    return n1/n2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -21,3 +24,6 @@ print(num1, "-", num2, "=", res)
 
 res = gop_func(num1, num2)
 print(num1, "*", num2, "=", res)
+
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
