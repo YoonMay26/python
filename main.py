9e6322ec9dc4b1d88730ff2de145eb2ad1cf2cad
YOON <ppom20152@gmail.com>
2024-10-24 14:30:12 +0900

    곱하기 함수 추가

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def gop_func(n1,n2):
+    return n1*n2
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
@@ -16,3 +18,6 @@ print(num1, "+", num2, "=", res)
 
 res = sub_func(num1, num2)
 print(num1, "-", num2, "=", res)
+
+res = gop_func(num1, num2)
+print(num1, "*", num2, "=", res)
